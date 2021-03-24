105041de1a7b65374ff700a07a74bee067c87df2
crypticani <aniket.kumar@hotmail.com>
2021-03-24 10:06:44 +0530

    restructured all models

--- predictionapp/admin.py
+++ predictionapp/admin.py
@@ -36,8 +36,8 @@ class TeamRegistrationA(admin.ModelAdmin):
 
 
 class PermanentTeamA(admin.ModelAdmin):
-    list_display = ['id', 'category', 'event_name', 'team_name', 'captain']
-    list_filter = ['category', 'event_name']
+    list_display = ['id', 'event_name', 'team_name', 'captain']
+    list_filter = ['event_name__category__category', 'event_name']
     search_fields = ['team_name', 'captain', 'vice_captain']
     inlines = [TeamPlayersI]
 
@@ -56,16 +56,16 @@ class TeamPlayersA(admin.ModelAdmin):
     #get_event_name.short_description = 'Event Name'  #Renames column head
 
     def get_category(self, obj):
-        return obj.team_id.category
+        return obj.team_id.id.category
     get_event_name.admin_order_field  = 'team_id'  #Allows column order sorting
     #get_event_name.short_description = 'Category'  #Renames column head
 
-    list_filter = ['team_id__category', 'team_id__event_name', 'team_id__team_name', 'is_active']
+    list_filter = ['team_id__event_name__category__category', 'team_id__event_name', 'team_id__team_name', 'is_active']
     search_fields = ['player_name', 'id_number']
 
 
 class TeamEventsListA(admin.ModelAdmin):
-    list_filter = ['event_name__events', 'category__category']
+    list_filter = ['event_name__events', 'event_name__category__category']
     list_display = ['id', 'team1', 'team2', 'datetime', 'venue']
 
     def get_team1(self, obj):
@@ -75,17 +75,18 @@ class TeamEventsListA(admin.ModelAdmin):
         return obj.team2
 
 class TeamRecordA(admin.ModelAdmin):
-    list_display = ['event_id', 'event_name', 'date', 'category', 'get_team1', 'get_team2', 'get_winner']
+    list_display = ['event_id', 'get_winner']
+    list_filter = ['event_id__event_name__category__category', 'winner']
 
-    def get_team1(self, obj):
-        return obj.team1
-    get_team1.admin_order_field  = 'record_id'  #Allows column order sorting
-    #get_team1.short_description = 'Team 1'  #Renames column head
+    # def get_team1(self, obj):
+    #     return obj.event_id.team1
+    # get_team1.admin_order_field  = 'record_id'  #Allows column order sorting
+    # #get_team1.short_description = 'Team 1'  #Renames column head
 
-    def get_team2(self, obj):
-        return obj.team2
-    get_team2.admin_order_field  = 'record_id'  #Allows column order sorting
-    #get_team2.short_description = 'Team 2'  #Renames column head
+    # def get_team2(self, obj):
+    #     return obj.event_id.team2
+    # get_team2.admin_order_field  = 'record_id'  #Allows column order sorting
+    # #get_team2.short_description = 'Team 2'  #Renames column head
 
     def get_winner(self, obj):
         return obj.winner
--- predictionapp/models.py
+++ predictionapp/models.py
@@ -6,6 +6,11 @@ class Categories(models.Model):
     category_id = models.AutoField(primary_key=True)
     category = models.CharField(max_length=10, default="Male")
 
+
+    class Meta:
+        verbose_name_plural = "Category"
+
+
     def __str__(self):
         return self.category
 
@@ -13,18 +18,18 @@ class Categories(models.Model):
 class TeamEvents(models.Model):
     tevent_id = models.AutoField(primary_key=True)
     events = models.CharField(blank=True, max_length=20)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
     active = models.BooleanField(default=True)
 
     class Meta:
         verbose_name_plural = "Team Events"
 
     def __str__(self):
-        return self.events
+        return str('{} {}'.format(self.events, self.category))
 
 
 class PermanentTeam(models.Model):
     id = models.IntegerField(primary_key=True)
-    category = models.ForeignKey(Categories, on_delete=models.SET_NULL, null=True)
     event_name = models.ForeignKey(TeamEvents, on_delete=models.CASCADE)
     team_name = models.CharField(max_length=30)
     captain = models.CharField(max_length=30)
@@ -36,7 +41,7 @@ class PermanentTeam(models.Model):
         verbose_name_plural = "Permanent Teams"
 
     def __str__(self):
-        return str('{} {} {}'.format(self.team_name, self.event_name, self.category))
+        return str('{} {}'.format(self.team_name, self.event_name))
     
 
 class TeamPlayers(models.Model):
@@ -70,7 +75,6 @@ class TeamRegistrationmodel(models.Model):
 class TeamEventList(models.Model):
     id = models.IntegerField(primary_key=True)
     event_name = models.ForeignKey(TeamEvents, on_delete=models.CASCADE)
-    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
     team1 = models.ForeignKey(TeamRegistrationmodel, related_name='team11', on_delete=models.CASCADE, limit_choices_to={'year': datetime.date.today().year})
     team2 = models.ForeignKey(TeamRegistrationmodel, related_name='team12', on_delete=models.CASCADE, limit_choices_to={'year': datetime.date.today().year})
     datetime = models.DateTimeField()
@@ -80,17 +84,12 @@ class TeamEventList(models.Model):
         verbose_name_plural = "Team Event List"
 
     def __str__(self):
-        return str('{} {}'.format(self.event_name, self.category))
+        return str('{} vs {}, {}'.format(self.team1, self.team2, self.datetime.date()))
 
 
 class TeamRecordModel(models.Model):
     record_id = models.AutoField(primary_key=True)
-    event_id = models.IntegerField()
-    event_name = models.ForeignKey(TeamEvents, on_delete=models.CASCADE)
-    date = models.DateField(blank=True, null=True)
-    category = models.ForeignKey(Categories, on_delete=models.SET_NULL, null=True)
-    team1 = models.ForeignKey(TeamRegistrationmodel, related_name='team1', on_delete=models.CASCADE, limit_choices_to={'year': datetime.date.today().year})
-    team2 = models.ForeignKey(TeamRegistrationmodel, related_name='team2', on_delete=models.CASCADE, limit_choices_to={'year': datetime.date.today().year})
+    event_id = models.ForeignKey(TeamEventList, on_delete=models.CASCADE)
     score_team1 = models.CharField(blank=True, max_length=30)
     score_team2 = models.CharField(blank=True, max_length=30)
     winner = models.ForeignKey(TeamRegistrationmodel, related_name='team3', on_delete=models.CASCADE, limit_choices_to={'year': datetime.date.today().year})
